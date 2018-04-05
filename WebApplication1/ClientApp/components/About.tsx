3f19bc1153dfe59bf7cd024b98e948afd299ba6e
Pablo Gramajo <pdgramajo@gmail.com>
2018-04-05 00:18:10 -0300

    Add project files.

---
+++ WebApplication1/ClientApp/components/About.tsx
@@ -0,0 +1,129 @@
+﻿import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../store';
+import * as AboutStore from '../store/About';
+
+
+type AboutProps =
+    AboutStore.AboutState
+    & typeof AboutStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class About extends React.Component<AboutProps, {}> {
+    public render() {
+        return <div>
+            <h1>Counter</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+            <p>Current count: <strong>{this.props.count}</strong></p>
+
+            <button onClick={() => { this.props.toogle() }}>showTable</button>
+
+            {this.props.showTable &&
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                Column heading
+                        </th>
+                            <th>
+                                Column heading
+                        </th>
+                            <th>
+                                Column heading
+                        </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="active">
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr className="success">
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr className="warning">
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                        <tr className="danger">
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                            <td>
+                                Column content
+                        </td>
+                        </tr>
+                    </tbody>
+                </table>
+            }
+
+        </div>;
+    }
+}
+
+// Wire up the React component to the Redux store
+export default connect(
+    (state: ApplicationState) => state.about, // Selects which state properties are merged into the component's props
+    AboutStore.actionCreators                 // Selects which action creators are merged into the component's props
+)(About) as typeof About;
---
+++ WebApplication1/ClientApp/store/About.ts
@@ -0,0 +1,50 @@
+﻿import { Action, Reducer } from 'redux';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface AboutState {
+    showTable: boolean;
+    count: number;
+}
+
+// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+// Use @typeName and isActionType for type detection that works even after serialization/deserialization.
+
+interface ToogleTableAction { type: 'TOOGLE_TABLE' }
+interface DecrementCountAction { type: 'DECREMENT_COUNT' }
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+type KnownAction = ToogleTableAction | DecrementCountAction;
+
+// ----------------
+// ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
+// They don't directly mutate state, but they can have external side-effects (such as loading data).
+
+export const actionCreators = {
+    toogle: () => <ToogleTableAction>{ type: 'TOOGLE_TABLE' },
+    decrement: () => <DecrementCountAction>{ type: 'DECREMENT_COUNT' }
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+const unloadedState: AboutState = {  showTable:true, count: 0 }
+
+export const reducer: Reducer<AboutState> = (state: AboutState, action: KnownAction) => {
+    switch (action.type) {
+        case 'TOOGLE_TABLE':
+            return { showTable: !state.showTable, count: state.count - 1 };
+        case 'DECREMENT_COUNT':
+            return { showTable: !state.showTable, count: state.count - 1 };
+        default:
+            // The following line guarantees that every action in the KnownAction union has been covered by a case above
+            const exhaustiveCheck: never = action;
+    }
+
+    // For unrecognized actions (or in cases where actions have no effect), must return the existing state
+    //  (or default initial state if none was supplied)
+    return state || unloadedState;
+};
